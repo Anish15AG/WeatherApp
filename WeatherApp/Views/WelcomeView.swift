30c89c695b235788ceb37b7ddc4854744f1da207
Anish <anish.agclg@gmail.com>
2025-04-09 15:10:49 +0530

    feat: Enhanced WelcomeView with implementation of Location Request UI

--- WeatherApp/Views/WelcomeView.swift
+++ WeatherApp/Views/WelcomeView.swift
@@ -6,14 +6,31 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
 struct WelcomeView: View {
-    
+     
     @EnvironmentObject var locationManager: LocationManager
     
     var body: some View {
-        VStack(spacing: 20){
-            Text("Welcome to the Weather App")
+        VStack{
+            VStack(spacing: 20){
+                Text("Welcome to the Weather App")
+                    .bold().font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+                Text("Please share your current location to get the weather in your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation){
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(/*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
+            
         }
         .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity )
     }
