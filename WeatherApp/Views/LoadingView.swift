32ae22d5e73a1d6a07f5d08c6476cce89f8f20a3
Anish <anish.agclg@gmail.com>
2025-04-10 04:12:59 +0530

    feat: Implemented loading screen post fetching location from user

---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Anish Gurusankar on 10/04/25.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
