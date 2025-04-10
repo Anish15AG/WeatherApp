cac857e0520f35909342651128772b7c20fc5e2e
Anish <anish.agclg@gmail.com>
2025-04-10 20:52:57 +0530

    feat: Added extension to round off the weather value

--- WeatherApp/Views/WeatherView.swift
+++ WeatherApp/Views/WeatherView.swift
@@ -10,7 +10,47 @@ import SwiftUI
 struct WeatherView: View {
     var weather: ResponseBody
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading){
+            
+            VStack{
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold().font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                
+                Spacer()
+                
+                VStack{
+                    HStack{
+                        VStack(spacing: 20){
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            
+                            Text(weather.weather[0].main)
+                             
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Text("\(weather.main.feels_like.roundDouble())" + "°")
+                            .font(.system(size: 100))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding()
+                    }
+                    
+                }
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.603, saturation: 0.886, brightness: 0.814))
+        .preferredColorScheme(.dark)
     }
 }
 
